483fe9afa6d7e9823690c4ea61f06829062adebf
misurac <rmisurac@gmail.com>
2021-03-26 20:32:32 -0500

    EventOrganizer seed data

--- EventCatalogAPI/Data/EventSeed.cs
+++ EventCatalogAPI/Data/EventSeed.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EventCatalogAPI.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,21 @@ namespace EventCatalogAPI.Data
 {
     public class EventSeed
     {
+        private static IEnumerable<EventOrganizer> GetEventOrganizers()
+        {
+            return new List<EventOrganizer>
+            {
+                new EventOrganizer { Coordinator = "Sam", Title = "Mr." },
+                new EventOrganizer { Coordinator = "Jamie", Title = "Ms." },
+                new EventOrganizer { Coordinator = "Jason", Title = "Mr." },
+                new EventOrganizer { Coordinator = "Matthew", Title = "Mr." },
+                new EventOrganizer { Coordinator = "Christian", Title = "Mrs." },
+                new EventOrganizer { Coordinator = "Sarah", Title = "Miss." },
+                new EventOrganizer { Coordinator = "Celeste", Title = "Miss." },
+                new EventOrganizer { Coordinator = "Joe", Title = "Mr." },
+                new EventOrganizer { Coordinator = "Michelle", Title = "Mrs." }
+
+            };
+        }
     }
 }
