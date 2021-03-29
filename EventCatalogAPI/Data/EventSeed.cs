7fdda9176d9e914a8c54f8cb642d1c19ed97f74f
LalithaNadimpalli <lalli.nadimpalli@gmail.com>
2021-03-28 17:09:41 -0700

    EventTypeSeeddata

--- EventCatalogAPI/Data/EventSeed.cs
+++ EventCatalogAPI/Data/EventSeed.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using EventCatalogAPI.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace EventCatalogAPI.Data
 {
-    public class EventSeed
+    public static class EventSeed
     {
+        private static IEnumerable<EventType> GetEventTypes()
+        {
+            return new List<EventType>()
+            {
+                new EventType() { Type= "Conference"},
+                new EventType() { Type= "VirtualEvent"},
+                new EventType() { Type= "SocialEvent"},
+                new EventType() { Type= "CorporateEvent"},
+                new EventType() { Type= "Party"},
+                new EventType() { Type= "CommunityEvent"},
+
+            };
+        }
+
     }
 }
