7e14ebe34934777593d9feb827d36ca838c23258
LalithaNadimpalli <lalli.nadimpalli@gmail.com>
2021-03-29 19:49:28 -0700

    EventTypeAPI

---
+++ EventCatalogAPI/Controllers/EventController.cs
@@ -0,0 +1,33 @@
+﻿using EventCatalogAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly EventContext _context;
+        private readonly IConfiguration _config;
+        public EventController(EventContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> EventTypes()
+        {
+            var events = await _context.EventTypes.ToListAsync();
+            return Ok(events);
+        }
+    }
+}
