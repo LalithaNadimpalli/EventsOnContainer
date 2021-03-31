99b522fc66b650dca8c996e074b88874174d4d37
misurac <rmisurac@gmail.com>
2021-03-30 21:07:26 -0500

    Created EventItems Controller

---
+++ EventCatalogAPI/Controllers/EventItemsController.cs
@@ -0,0 +1,24 @@
+﻿using EventCatalogAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventItemsController : ControllerBase
+    {
+        private readonly EventContext _context;
+        private readonly IConfiguration _config;
+        public EventItemsController(EventContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+    }
+}
