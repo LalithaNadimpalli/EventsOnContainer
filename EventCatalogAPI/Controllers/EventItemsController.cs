5700cf4e8c076567493f1eaa29547fdfdc732731
misurac <rmisurac@gmail.com>
2021-03-30 21:17:55 -0500

    Added date filtering APIs to EventItemsController

--- EventCatalogAPI/Controllers/EventItemsController.cs
+++ EventCatalogAPI/Controllers/EventItemsController.cs
@@ -1,6 +1,7 @@
 ﻿using EventCatalogAPI.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
@@ -20,5 +21,47 @@ namespace EventCatalogAPI.Controllers
             _context = context;
             _config = config;
         }
+
+        //Re-orders events by date - oldest to newest
+        [HttpGet("action")]
+        public async Task<IActionResult> Dates()
+        {
+            var events = await _context.EventItems
+                .OrderBy(d => d.EventStartTime)
+                .ToListAsync();
+
+            return Ok(events);
+        }
+
+        //Sorts event by month
+        [HttpGet("action")]
+        public async Task<IActionResult> FilterByMonth(
+            [FromQuery] string month)
+        {
+            var events = await _context.EventItems
+                .Where(d => d.EventStartTime.ToString("MMMM") == month)
+                .OrderBy(d => d.EventStartTime)
+                .ToListAsync();
+
+            return Ok(events);
+        }
+
+        //filters events by specific date
+        [HttpGet("action")]
+        public async Task<IActionResult> FilterByDate(
+            [FromQuery] string month,
+            [FromQuery] int day,
+            [FromQuery] int year)
+        {
+            var events = await _context.EventItems
+                .Where(d => d.EventStartTime.ToString("MMMM") == month)
+                .Where(d => d.EventStartTime.Day == day)
+                .Where(d => d.EventStartTime.Year == year)
+                .OrderBy(d => d.EventStartTime)
+                .ToListAsync();
+
+            return Ok(events);
+        }
+
     }
 }
