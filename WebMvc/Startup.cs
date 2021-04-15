9a28277048610374d096dc6bdeafd1240395bb72
shegole23 <shegole23@users.noreply.github.com>
2021-04-14 18:26:29 -0700

    tokenapi dockerized

--- WebMvc/Startup.cs
+++ WebMvc/Startup.cs
@@ -34,67 +34,66 @@ namespace WebMvc
             services.AddSingleton<IHttpClient, CustomHttpClient>();
             services.AddTransient<IEventService, EventService>();
             //services.AddTransient<IIdentityService<ApplicationUser>, IdentityService>();
-            //var identityUrl = Configuration.GetValue<string>("IdentityUrl");
-            //var callBackUrl = Configuration.GetValue<string>("CallBackUrl");
-            //services.AddAuthentication(options =>
-            //{
-            //    options.DefaultScheme = "Cookies";
-            //    options.DefaultChallengeScheme = "oidc";
+            var identityUrl = Configuration.GetValue<string>("IdentityUrl");
+            var callBackUrl = Configuration.GetValue<string>("CallBackUrl");
+            services.AddAuthentication(options =>
+            {
+                
 
-            //    //options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-            //    //options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-            //    //options.DefaultAuthenticateScheme = "Cookies";
-            //})
-            //.AddCookie("Cookies")
-            //.AddOpenIdConnect("oidc", options =>
-            //{
-            //    options.SignInScheme = "Cookies";
-            //    //options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+               
+            })
+            .AddCookie("Cookies")
+            .AddOpenIdConnect("oidc", options =>
+            {
+                options.SignInScheme = "Cookies";
+                options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
 
-            //    options.Authority = identityUrl.ToString();
-            //    options.SignedOutRedirectUri = callBackUrl.ToString();
-            //    options.ClientId = "mvc";
-            //    options.ClientSecret = "secret";
-            //    options.ResponseType = "code id_token";
-            //    options.SaveTokens = true;
-            //    options.GetClaimsFromUserInfoEndpoint = true;
-            //    options.RequireHttpsMetadata = false;
-            //    options.Scope.Add("openid");
-            //    options.Scope.Add("profile");
-            //    options.Scope.Add("offline_access");
-            //    options.TokenValidationParameters = new TokenValidationParameters()
-            //    {
+                options.Authority = identityUrl.ToString();
+                options.SignedOutRedirectUri = callBackUrl.ToString();
+                options.ClientId = "mvc";
+                options.ClientSecret = "secret";
+                options.ResponseType = "code id_token";
+                options.SaveTokens = true;
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.RequireHttpsMetadata = false;
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                options.Scope.Add("offline_access");
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
 
-            //        NameClaimType = "name",
-            //        RoleClaimType = "role"
-            //    };
-
+                    NameClaimType = "name",
+                    RoleClaimType = "role"
+                };
 
 
-            //    });
+
+                   });
             }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-            //else
-            //{
-            //    app.UseExceptionHandler("/Home/Error");
-            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            //    app.UseHsts();
-            //}
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
             app.UseDeveloperExceptionPage();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
-           // app.UseCookiePolicy();
+            app.UseCookiePolicy();
             app.UseAuthentication();
-           // app.UseAuthorization();
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
                 {
                     endpoints.MapControllerRoute(
