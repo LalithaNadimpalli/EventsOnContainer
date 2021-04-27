2a3294d427f1b2e3987273958b232100b7834acd
SemraChernet <61530532+SemraChernet@users.noreply.github.com>
2021-04-27 13:42:51 -0700

    Update ICartService.cs

--- WebMvc/Services/ICartService.cs
+++ WebMvc/Services/ICartService.cs
@@ -1,24 +1,23 @@
-﻿using WebMvc.Models;
+using WebMvc.Models;
 using WebMvc.Models.CartModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebMvc.Models.OrderModels;
 //using WebMvc.Models.OrderModels;
-
 
 namespace WebMvc.Services
 {
     public interface ICartService
     {
         Task<Cart> GetCart(ApplicationUser user);
-        Task AddItemToCart(ApplicationUser user, CartEventItem event);
-
+        Task AddItemToCart(ApplicationUser user, CartItem product);
         Task<Cart> UpdateCart(Cart Cart);
         Task<Cart> SetQuantities(ApplicationUser user, Dictionary<string, int> quantities);
-        
-        //Order MapCartToOrder(Cart Cart);
+        Order MapCartToOrder(Cart Cart);
         Task ClearCart(ApplicationUser user);
-       
+
     }
 }
+
