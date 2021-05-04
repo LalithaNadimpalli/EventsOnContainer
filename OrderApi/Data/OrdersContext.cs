b83510cf15d0d4a1513e3647733065bc5c1ad562
LalithaNadimpalli <63084352+LalithaNadimpalli@users.noreply.github.com>
2021-05-04 07:23:03 -0700

    DataFolder
    Datafolder

---
+++ OrderApi/Data/OrdersContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderApi.Data
+{
+    public class OrdersContext : DbContext
+    {
+        public OrdersContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>(e =>
+            {
+                e.Property(o => o.OrderId)
+                    .IsRequired()
+                    .ValueGeneratedOnAdd();
+
+            });
+            modelBuilder.Entity<OrderItem>(e =>
+            {
+                e.Property(o => o.Id)
+                    .IsRequired()
+                    .ValueGeneratedOnAdd();
+
+                e.HasOne(o => o.Order)
+                .WithMany()
+                .HasForeignKey(o => o.OrderId);
+
+            });
+
+        }
+    }
+
+}
